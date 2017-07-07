0bccbd4a094ad9fd28dba061ce86b97516e4ab4d
Kazuki Ota <kaota@microsoft.com>
2017-07-07 16:43:31 +0900

    UnitTest

---
+++ DITestBotApp.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DITestBotApp.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DITestBotApp.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b04411e2-e4be-4df4-9be7-3bd02f9929c1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DITestBotApp.Test/RootDialogTest.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DITestBotApp.Dialogs;
+using Microsoft.Bot.Builder.Dialogs;
+using Moq;
+using System.Threading.Tasks;
+using Microsoft.Bot.Connector;
+using DITestBotApp.Services;
+using DITestBotApp.Factories;
+using System.Threading;
+
+namespace DITestBotApp.Test
+{
+    [TestClass]
+    public class RootDialogTest
+    {
+        [TestMethod]
+        public async Task StartAsyncTest()
+        {
+            var target = new RootDialog(null, null);
+
+            var dialogContextMock = new Mock<IDialogContext>();
+            dialogContextMock.Setup(x => x.Wait(It.Is<ResumeAfter<IMessageActivity>>(y => y.Method.Name == nameof(RootDialog.GreetInteractionAsync))))
+                .Verifiable();
+
+            await target.StartAsync(dialogContextMock.Object);
+            dialogContextMock.Verify();
+        }
+
+        [TestMethod]
+        public async Task GreetInteractionAsyncTest()
+        {
+            var dialogFactoryMock = new Mock<IDialogFactory>();
+            var greetServiceMock = new Mock<IGreetService>();
+            greetServiceMock.Setup(x => x.GetMessage())
+                .Returns("Hello world")
+                .Verifiable();
+
+            var dialogContextMock = new Mock<IDialogContext>();
+            dialogContextMock.Setup(x => x.MakeMessage())
+                .Returns(Activity.CreateMessageActivity());
+            dialogContextMock.Setup(x => x.PostAsync(It.Is<IMessageActivity>(y => y.Text == "Hello world"), default(CancellationToken)))
+                .Returns(Task.CompletedTask)
+                .Verifiable();
+            dialogContextMock.Setup(x => x.PostAsync(It.Is<IMessageActivity>(y => y.Text == "You sent okazuki which was 7 characters"), default(CancellationToken)))
+                .Returns(Task.CompletedTask)
+                .Verifiable();
+            var resultMock = Awaitable.FromItem<object>(new Activity { Text = "okazuki" });
+
+            var target = new RootDialog(dialogFactoryMock.Object, greetServiceMock.Object);
+            await target.GreetInteractionAsync(dialogContextMock.Object, resultMock);
+
+            greetServiceMock.Verify();
+            dialogContextMock.Verify();
+        }
+
+        [TestMethod]
+        public async Task MainInteractionAsyncChangeCase()
+        {
+            var dialogFactoryMock = new Mock<IDialogFactory>();
+            var simpleDialog = new SimpleDialog();
+            dialogFactoryMock.Setup(x => x.Create<SimpleDialog>())
+                .Returns(simpleDialog)
+                .Verifiable();
+
+            var dialogContextMock = new Mock<IDialogContext>();
+            dialogContextMock.Setup(x => x.Call(It.Is<SimpleDialog>(y => y == simpleDialog),
+                It.Is<ResumeAfter<object>>(y => y.Method.Name == nameof(RootDialog.ReturnFromSimpleDialogInteractionAsync))))
+                .Verifiable();
+
+            var resultMock = Awaitable.FromItem<object>(new Activity { Text = "change" });
+
+            var target = new RootDialog(dialogFactoryMock.Object, null);
+            await target.MainInteractionAsync(dialogContextMock.Object, resultMock);
+
+            dialogFactoryMock.Verify();
+            dialogContextMock.Verify();
+        }
+
+        [TestMethod]
+        public async Task ReturnFromSimpleDialogInteractionAsync()
+        {
+            var dialogContextMock = new Mock<IDialogContext>();
+            dialogContextMock.Setup(x => x.PostAsync(It.Is<IMessageActivity>(y => y.Text == "returned"), default(CancellationToken)))
+                .Returns(Task.CompletedTask)
+                .Verifiable();
+
+            dialogContextMock.Setup(x => x.MakeMessage())
+                .Returns(Activity.CreateMessageActivity());
+            dialogContextMock.Setup(x => x.Wait(It.Is<ResumeAfter<IMessageActivity>>(y => y.Method.Name == nameof(RootDialog.MainInteractionAsync))))
+                .Verifiable();
+
+            var target = new RootDialog(null, null);
+            await target.ReturnFromSimpleDialogInteractionAsync(dialogContextMock.Object, null);
+
+            dialogContextMock.Verify();
+        }
+    }
+}
